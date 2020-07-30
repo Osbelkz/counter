7a854128f0b73e9e804b08797490229a42ae670d
Askar <osbelkz@gmail.com>
2020-07-30 17:27:40 +0600

    Updates

---
+++ precache-manifest.f11a90ab82658f0bd3bdd4ddd9fc9baa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e699392d6e440fff71e7a019b7452d2",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "da65b9e5292434177af0",
+    "url": "/counter/static/css/main.566bfe90.chunk.css"
+  },
+  {
+    "revision": "b126613e2cc848364261",
+    "url": "/counter/static/js/2.18f56b61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter/static/js/2.18f56b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da65b9e5292434177af0",
+    "url": "/counter/static/js/main.5881b031.chunk.js"
+  },
+  {
+    "revision": "38577f72a7927ff16652",
+    "url": "/counter/static/js/runtime-main.9633a9c1.js"
+  }
+]);
